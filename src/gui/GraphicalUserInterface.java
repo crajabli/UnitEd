1fe570e41ceb877438406afd73d2b2aac8575ce2
maken <maken@172.30.10.135>
2022-04-04 15:36:45 -0400

    added in line comments, added unfinished reset and clear methods

--- src/gui/GraphicalUserInterface.java
+++ src/gui/GraphicalUserInterface.java
@@ -1,21 +1,22 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Frame;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 
+/**
+ * 
+ * @author Makenzie Williams
+ * @version 1
+ *
+ */
 public class GraphicalUserInterface implements ActionListener
 {
 
@@ -34,45 +35,46 @@ public class GraphicalUserInterface implements ActionListener
   private void setLayout()
   {
 
+    // creation of frame and content pane
     JFrame frame = new JFrame("UnitED");
     JPanel contentPane = (JPanel)frame.getContentPane();
     contentPane.setLayout(new GridLayout(3,1));
     
-    
+    // creation of panels
     JPanel displayPanel = new JPanel();
     displayPanel.setLayout(new BorderLayout());
     JPanel inputPanel = new JPanel();
     inputPanel.setLayout(new BorderLayout());
     JPanel buttonPanel = new JPanel();
     
+    // adding panels to content frame
     contentPane.add(displayPanel);
     contentPane.add(inputPanel);
     contentPane.add(buttonPanel);
     
+    // creation of display text field
     JTextField display = new JTextField();
     display.setEditable(false);
     display.setHorizontalAlignment(JTextField.CENTER);
 
+    // creation of input text field
     JTextField input = new JTextField();
     display.setSize(500, 50);
-
+    
+    // creation of all buttons
     JButton reset = new JButton("R");
-    
     JButton clear = new JButton("C");
-    
     JButton plus = new JButton("+");
-    
     JButton minus = new JButton("-");
-    
     JButton multiply = new JButton("x");
-    
     JButton divide = new JButton("\u00F7");
-    
     JButton equals = new JButton("=");
     
+    // adding display and input to respective panels
     displayPanel.add(display, BorderLayout.CENTER);
     inputPanel.add(input, BorderLayout.CENTER);
     
+    // adding buttons to button panel
     buttonPanel.add(reset);
     buttonPanel.add(clear);
     buttonPanel.add(plus);
@@ -80,10 +82,24 @@ public class GraphicalUserInterface implements ActionListener
     buttonPanel.add(multiply);
     buttonPanel.add(divide);
     buttonPanel.add(equals);
-
-
+    
+    // setting frame
     frame.setSize(550, 250);
     frame.setVisible(true);
+  }
+  
+  /**
+   * Clears both input and display field.
+   */
+  private void reset() {
+    
+  }
+  
+  /**
+   * Clears input field.
+   */
+  private void clear() {
+   
   }
 
 }
