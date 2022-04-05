4c828d5bdbc5be0156370278aa2f95a9980d7cee
aten-jl <atenjl@dukes.jmu.edu>
2022-04-05 09:36:50 -0400

    Added custom exception

---
+++ src/utilities/OperationFormatException.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class OperationFormatException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public OperationFormatException(String error)
+	{
+		super(error);
+	}
+
+}
