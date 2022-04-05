ec2a67ea75d2a7618fa310403f4da33b709c5df3
aten-jl <atenjl@dukes.jmu.edu>
2022-04-05 09:52:49 -0400

    Added functionality for add/subtract/multiply/divide methods

--- src/utilities/Operation.java
+++ src/utilities/Operation.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
  * Calculates result given two operands and an operator.
  * 
  * @author Victor Aten
- * @version 4/4/22
+ * @version 4/5/22
  */
 public class Operation {
 	/**
@@ -50,13 +50,13 @@ public class Operation {
 	 * @param leftOp represents left operand
 	 * @param rightOp represents right operand
 	 * @return String
-	 * @throws OperationFormatException 
+	 * @throws OperationFormatException when units are not the same
 	 */
 	public String add(final Operand leftOp, final Operand rightOp) throws OperationFormatException 
 	{
 		sameUnitException(leftOp, rightOp);
 		BigDecimal value = leftOp.getValue().add(rightOp.getValue());
-		return "";
+		return value + " " + leftOp.getUnit(); // Assumes that leftOp and rightOp unit are the same
 	}
 
 	/**
@@ -65,13 +65,13 @@ public class Operation {
 	 * @param leftOp represents left operand
 	 * @param rightOp represents right operand
 	 * @return String
-	 * @throws OperationFormatException 
+	 * @throws OperationFormatException when units are not the same
 	 */
 	public String subtract(final Operand leftOp, final Operand rightOp) throws OperationFormatException 
 	{
 		sameUnitException(leftOp, rightOp);
 		BigDecimal value = leftOp.getValue().subtract(rightOp.getValue());
-		return "";
+		return value + " " + leftOp.getUnit(); // Assumes that leftOp and rightOp unit are the same
 	}
 
 	/**
@@ -83,7 +83,8 @@ public class Operation {
 	 */
 	public String multiply(final Operand leftOp, final Operand rightOp) 
 	{
-		return "";
+		BigDecimal value = leftOp.getValue().multiply(rightOp.getValue());
+		return value + " " + leftOp.getUnit() + "-" + rightOp.getUnit();
 	}
 
 	/**
@@ -92,13 +93,27 @@ public class Operation {
 	 * @param leftOp represents left operand
 	 * @param rightOp represents right operand
 	 * @return String
+	 * @throws OperationFormatException when trying to divide by zero
 	 */
-	public String divide(final Operand leftOp, final Operand rightOp) 
+	public String divide(final Operand leftOp, final Operand rightOp) throws OperationFormatException 
 	{
-		// check that rightOp value is not zero
-		return "";
+		// Check that rightOp value is not zero
+		if (rightOp.getValue().equals(0))
+		{
+			throw new OperationFormatException("Cannot divide by zero");
+		}
+		
+		BigDecimal value = leftOp.getValue().divide(rightOp.getValue());
+		return value + " " + leftOp.getUnit() + "/" + rightOp.getUnit();
 	}
 	
+	/**
+	 * Helper method to throw exception.
+	 * 
+	 * @param leftOp represents left operand
+	 * @param rightOp represents right operand
+	 * @throws OperationFormatException when units are the same
+	 */
 	private void sameUnitException(final Operand leftOp, final Operand rightOp) throws OperationFormatException
 	{
 		if (!leftOp.getUnit().equals(rightOp.getUnit()))
