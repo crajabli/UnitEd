ce44ea69a97999842a770722a397c380584c9416
makenzie williams <willi9mj@dukes.jmu.edu>
2022-04-17 14:57:55 -0400

    modified setLayout()

--- src/gui/GraphicalUserInterface.java
+++ src/gui/GraphicalUserInterface.java
@@ -4,7 +4,7 @@ import utilities.Operation;
 import utilities.OperationFormatException;
 
 import java.awt.BorderLayout;
-
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,6 +24,8 @@ public class GraphicalUserInterface implements ActionListener
   JTextField input;
   String[] expression = new String[3];
   private final String DIVIDE = "\u00F7";
+  private final String SIGN = "\u00B1";
+  private final String BACK = "\u232B";
 
   /**
    * Constructor.
@@ -105,10 +107,8 @@ public class GraphicalUserInterface implements ActionListener
         catch (NumberFormatException ne)
         {
           reset();
-          JOptionPane.showMessageDialog(null,
-              "You entered a negative number  ", "negative number ",
-              JOptionPane.INFORMATION_MESSAGE
-          );
+          JOptionPane.showMessageDialog(null, "You entered a negative number  ", "negative number ",
+              JOptionPane.INFORMATION_MESSAGE);
         }
         catch (IllegalArgumentException ie)
         {
@@ -135,7 +135,6 @@ public class GraphicalUserInterface implements ActionListener
               JOptionPane.INFORMATION_MESSAGE);
         }
 
-
         clear();
 
       }
@@ -146,36 +145,102 @@ public class GraphicalUserInterface implements ActionListener
   /**
    * sets up the layout.
    */
+  @SuppressWarnings("rawtypes")
   private void setLayout()
   {
 
     // creation of frame and content pane
     JFrame frame = new JFrame("UnitED");
     JPanel contentPane = (JPanel) frame.getContentPane();
-    contentPane.setLayout(new GridLayout(3, 1));
+    contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
     // creation of panels
+    JPanel topExteriorPanel = new JPanel();
+    topExteriorPanel.setLayout(new BoxLayout(topExteriorPanel, BoxLayout.Y_AXIS));
+
+    JPanel bottomExteriorPanel = new JPanel();
+    bottomExteriorPanel.setLayout(new BoxLayout(bottomExteriorPanel, BoxLayout.X_AXIS));
+
+    JPanel logoPanel = new JPanel();
+    logoPanel.setLayout(new BorderLayout());
+
     JPanel displayPanel = new JPanel();
     displayPanel.setLayout(new BorderLayout());
+
     JPanel inputPanel = new JPanel();
-    inputPanel.setLayout(new BorderLayout());
-    JPanel buttonPanel = new JPanel();
+    inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.X_AXIS));
+
+    JPanel numberPanel = new JPanel();
+    numberPanel.setLayout(new BoxLayout(numberPanel, BoxLayout.Y_AXIS));
+
+    JPanel numberPanelOne = new JPanel();
+    JPanel numberPanelTwo = new JPanel();
+    JPanel numberPanelThree = new JPanel();
+    JPanel numberPanelFour = new JPanel();
+    JPanel numberPanelFive = new JPanel();
+
+    JPanel operatorPanel = new JPanel();
+    operatorPanel.setLayout(new BoxLayout(operatorPanel, BoxLayout.Y_AXIS));
+
+    JPanel plusPanel = new JPanel();
+    JPanel minusPanel = new JPanel();
+    JPanel multiplyPanel = new JPanel();
+    JPanel dividePanel = new JPanel();
+    JPanel equalsPanel = new JPanel();
+
+    JPanel historyPanel = new JPanel();
+
+    // creation of image label
+    ImageIcon image = new ImageIcon("src/unitED_Logo.png");
+    JLabel label = new JLabel(image);
+    logoPanel.add(label);
+
+    // creation of options in dropdown menu
+    String[] measurements = {"none", "c", "cm", "cm-cm", "ft", "ft-ft", "ft-ft-ft", "gal", "gr", "hrs", "in", "kg", "km", "l", "lbs",
+        "m", "mg", "mi", "mi-mi", "min", "mm", "mph", "oz", "pt", "qt", "sec", "sec-sec", "tbsp", "tsp", "yd"};
+
+    // creation of drop down menu
+    @SuppressWarnings("unchecked")
+    JComboBox dropdown = new JComboBox(measurements);
+    dropdown.setEditable(true);
+    dropdown.setVisible(true);
+
+    // adding interior panels to exterior panels
+    topExteriorPanel.add(logoPanel);
+    topExteriorPanel.add(displayPanel);
+    topExteriorPanel.add(inputPanel);
+
+    numberPanel.add(numberPanelOne);
+    numberPanel.add(numberPanelTwo);
+    numberPanel.add(numberPanelThree);
+    numberPanel.add(numberPanelFour);
+    numberPanel.add(numberPanelFive);
+
+    operatorPanel.add(plusPanel);
+    operatorPanel.add(minusPanel);
+    operatorPanel.add(multiplyPanel);
+    operatorPanel.add(dividePanel);
+    operatorPanel.add(equalsPanel);
+
+    bottomExteriorPanel.add(numberPanel);
+    bottomExteriorPanel.add(operatorPanel);
+    bottomExteriorPanel.add(historyPanel);
 
     // adding panels to content frame
-    contentPane.add(displayPanel);
-    contentPane.add(inputPanel);
-    contentPane.add(buttonPanel);
+    contentPane.add(topExteriorPanel);
+    contentPane.add(bottomExteriorPanel);
 
     // creation of display text field
     display = new JTextField();
     display.setEditable(false);
     display.setHorizontalAlignment(JTextField.CENTER);
+    display.setSize(500, 50);
 
     // creation of input text field
     input = new JTextField();
-    display.setSize(500, 50);
 
-    // creation of all buttons
+    // creation of utility, operator, and history buttons
+    JButton sign = new JButton(SIGN);
     JButton reset = new JButton("R");
     JButton clear = new JButton("C");
     JButton plus = new JButton("+");
@@ -183,17 +248,54 @@ public class GraphicalUserInterface implements ActionListener
     JButton multiply = new JButton("x");
     JButton divide = new JButton(DIVIDE);
     JButton equals = new JButton("=");
+    JButton history = new JButton(">");
+    JButton backspace = new JButton(BACK);
 
-    // adding buttons to button panel
-    buttonPanel.add(reset);
-    buttonPanel.add(clear);
-    buttonPanel.add(plus);
-    buttonPanel.add(minus);
-    buttonPanel.add(multiply);
-    buttonPanel.add(divide);
-    buttonPanel.add(equals);
+    // creation of number buttons
+    JButton zero = new JButton("0");
+    JButton one = new JButton("1");
+    JButton two = new JButton("2");
+    JButton three = new JButton("3");
+    JButton four = new JButton("4");
+    JButton five = new JButton("5");
+    JButton six = new JButton("6");
+    JButton seven = new JButton("7");
+    JButton eight = new JButton("8");
+    JButton nine = new JButton("9");
+
+    // adding utility buttons to panel
+    numberPanelOne.add(sign);
+    numberPanelOne.add(clear);
+    numberPanelOne.add(reset);
+
+    // adding number buttons to panels
+    numberPanelTwo.add(seven);
+    numberPanelTwo.add(eight);
+    numberPanelTwo.add(nine);
+
+    numberPanelThree.add(four);
+    numberPanelThree.add(five);
+    numberPanelThree.add(six);
+
+    numberPanelFour.add(one);
+    numberPanelFour.add(two);
+    numberPanelFour.add(three);
+
+    numberPanelFive.add(zero);
+    numberPanelFive.add(backspace);
+
+    // adding operator buttons to panel
+    plusPanel.add(plus);
+    minusPanel.add(minus);
+    multiplyPanel.add(multiply);
+    dividePanel.add(divide);
+    equalsPanel.add(equals);
+
+    // adding history button to panel
+    historyPanel.add(history);
 
     // adding action listener to each button
+    sign.addActionListener(this);
     reset.addActionListener(this);
     clear.addActionListener(this);
     plus.addActionListener(this);
@@ -201,15 +303,27 @@ public class GraphicalUserInterface implements ActionListener
     multiply.addActionListener(this);
     divide.addActionListener(this);
     equals.addActionListener(this);
+    history.addActionListener(this);
+    backspace.addActionListener(this);
+    zero.addActionListener(this);
+    one.addActionListener(this);
+    two.addActionListener(this);
+    three.addActionListener(this);
+    four.addActionListener(this);
+    five.addActionListener(this);
+    six.addActionListener(this);
+    seven.addActionListener(this);
+    eight.addActionListener(this);
+    nine.addActionListener(this);
 
     // adding display and input to respective panels
     displayPanel.add(display, BorderLayout.CENTER);
     inputPanel.add(input, BorderLayout.CENTER);
+    inputPanel.add(dropdown);
 
     // setting frame
     frame.setSize(550, 250);
     frame.setVisible(true);
-
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 
