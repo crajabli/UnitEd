7764ceedd489d40d4cbcde821cc514d4933b888b
makenzie williams <willi9mj@dukes.jmu.edu>
2022-04-20 10:42:18 -0400

    removed unused import

--- src/gui/GraphicalUserInterface.java
+++ src/gui/GraphicalUserInterface.java
@@ -4,7 +4,6 @@ import utilities.Operation;
 import utilities.OperationFormatException;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
