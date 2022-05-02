7b7c192473e8b4ad27d905ea48f713409b455e2d
aten-jl <atenjl@dukes.jmu.edu>
2022-05-02 15:17:14 -0400

    Started adding unit conversion tests

---
+++ src/testing/MoneyTest.java
@@ -0,0 +1,28 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.DivideByZeroException;
+import utilities.OperationFormatException;
+
+class MoneyTest {
+
+	@Test
+	void testToCent() throws OperationFormatException, DivideByZeroException {
+		// Both c
+		
+		// c to $
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testToDollar() throws OperationFormatException, DivideByZeroException {
+		// Both $
+		
+		// $ to c
+		fail("Not yet implemented");
+	}
+
+}
